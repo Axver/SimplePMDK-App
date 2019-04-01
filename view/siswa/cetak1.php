6684ced468edefd6db095f771cf2b4f1065eb0f0
unknown <ecimoramora@gmail.com>
2019-04-01 18:40:05 +0700

    Print Area

--- view/siswa/cetak1.php
+++ view/siswa/cetak1.php
@@ -1,28 +1,60 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>MEMBUAT CETAK PRINT LAPORAN DENGAN PHP - WWW.MALASNGODING.COM</title>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    
 </head>
 <body>
 
-	<center>
-		<h2>TUTORIAL CETAK PRINT LAPORAN DENGAN PHP</h2>
-		<h4>CONTOH LAPORAN YANG DI PRINT - WWW.MALASNGODING.COM</h4>
-	</center>
+<div class='container'>
 
-	<br/>
 
-	<p>
-		Tutorial membuat cetak print laporan dengan php. pada tutorial ini kita akan belajar cara membuat cetak laporan pada PHP dengan cara paling mudah.
-	</p>
+<div class='row'>
+<div class='col-sm-1'></div>
+<div class='col-sm-1'>
+<img style='height:100px;' src="file/logo.jpg" alt="" class='img-responsive img-rounded'>
+</div>
 
-	<p>
-		Ini adalah contoh data yang diprint pada tutorial <b>MEMBUAT CETAK PRINT LAPORAN DENGAN PHP</b> dari <b>www.malasngoding.com</b>, halaman ini akan dicetak sesuai dengan format HTML yang terdapat dalam file cetak1.php ini.
-	</p>
+<div class='col-sm-4'><b>Penelurusan Minat dan Kemampuan</b>
+<br>
+<small>Politeknik Se-Indonesia</small>
 
-	<script>
-		window.print();
+</div>
+<div class='col-sm-3'></div>
+<div class='col-sm-2'>
+<img style='height:100px;' src="file/tutwuri.png" alt="">
+</div>
+
+
+
+</div>
+
+<div class='row'>
+<div class='col-sm-1'>
+</div>
+<div class='col-sm-10'>
+<!-- Horizontal Line -->
+<hr style='color:blue; background-color:blue;' size="40">
+
+
+</div>
+
+<div class='col-sm-1'></div>
+</div>
+</div>
+</div>
+
+
+<script>
+		// window.print();
 	</script>
-	
 </body>
 </html>
